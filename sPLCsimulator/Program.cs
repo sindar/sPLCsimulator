74390bb5c8e9ece67887768f3ca6222b0c47b3b0
sindar <sindar@cgx.ru>
2020-08-03 23:25:20 +0300

    Add running a program(intended with read/write registes logic) as argument

--- sPLCsimulator/Program.cs
+++ sPLCsimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using zModBusTCPServer;
 
 namespace sPLCsimulator
@@ -10,22 +11,34 @@ namespace sPLCsimulator
 
         static void Main(string[] args)
         {
-            //InitRegs();
             ModBusTCPServer MBTCPServer = new ModBusTCPServer(HoldingRegs, InputRegs);
             MBTCPServer.StartServer();
             Console.WriteLine("ModBusTCP server is started: " + DateTime.Now);
+
+            if(args.Length > 0)
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "nohup",
+                        Arguments = args[0],
+                        UseShellExecute = true,
+                        RedirectStandardOutput = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+                Console.WriteLine("File {0} is started", args[0]);
+            }
+
             for (;;)
             {
                 ++HoldingRegs[0];
                 --InputRegs[0];
                 MBTCPServer.ProcessWriteQueue();
             }
-        }
-
-        static void InitRegs()
-        {
-            for (int i = 0; i < 65536; ++i)
-                HoldingRegs[i] = (ushort)(i + 1);
         }
     }
 }
